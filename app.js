6613305bd0c0cb9b1df69c1bc4b558ef533e5407
i.maslakoff <igormaslakoff@gmail.com>
2021-04-08 20:43:19 +0300

    add chalk library

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-console.log('Hello from JS');
+// console.log('Hello from JS');
+
+let firstName = 'Igor';
+let lastName = '   Maslakov   ';
+ 
+// charAt(index)
+// const symb = firstName.charAt(0);
+// console.log('Char at 0:', symb);
+// concat
+// const fullName = firstName.concat(' ', lastName);
+// console.log(fullName)
+const fullNamePlus = firstName + ' ' + lastName;
+// console.log('+: ', fullNamePlus);
+const fullNameTemplate = `${firstName} ${lastName}`;
+// console.log('fullNameTemplate: ', fullNameTemplate);
+
+const data = [];
+data.push(firstName);
+data.push('maslakoff');
+data.push(lastName);
+
+
+console.log('data:', data)
+
+const result = data
+.filter(elememt => typeof elememt === 'string')
+.map(item => item.trim())
+.join(' ');
+
+// const filteredData = data
+// .filter(elememt => typeof elememt === 'string');
+
+// const trimmedData = filteredData.map(item => item.trim());
+
+// const finalResult = trimmedData.join(' ');
+
+console.log('string data:', result)
+// indexOf
+// includes (es6)
+// replace
+// toUpperCase
+// console.log('Upper:', firstName.toUpperCase());
+// toLowerCase, 
+// console.log('Lower:', lastName.toLowerCase());
+// trim, 
+// console.log('trim:', lastName.trim());
+// split, 
+// const bill = '10$ 20$ 55$';
+// const billsArray = bill.split('$ ');
+// console.log('billsArray: ', billsArray)
+// lastIndexOf, 
+// slice, 
+// substring(start, end), 
+// substr(start, length), 
+// startsWith (es6), 
+// endsWith (es6), 
+// repeat (es6), 
